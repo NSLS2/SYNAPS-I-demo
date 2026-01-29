ad4541ed9574e8a167315c932ee93ef1b4eec6f3
skarakuzu <karakuzu.seher@gmail.com>
2026-01-29 15:58:45 -0500

    add different containers for recon and seg steps

--- producer.py
+++ producer.py
@@ -2,8 +2,8 @@ import numpy as np
 import time
 from tiled.client import from_uri
 
-client = from_uri('https://tiled-staging.nsls2.bnl.gov')
-pt = client['tst/sandbox/ptycho_test']
+client = from_uri('https://tiled.nsls2.bnl.gov')
+pt = client['tst/sandbox/synaps/reconstructions']
 
 shape = (50, 50)  # array dimensions
 N = 1  # number of updates
@@ -11,7 +11,7 @@ interval = 1  # delay between updates (seconds)
 
 arr = np.random.random(shape) + 1j * np.random.random(shape)
 print("Initial write... may be slow as libraries are imported for the first time.")
-arr_client = pt.write_array(arr)
+arr_client = pt.write_array(arr, access_tags=['tst_sandbox'])
 print(f"Created array dataset {arr_client.item['id']}")
 print(arr_client)
 uri = "Hello Mars!!!"
@@ -19,5 +19,4 @@ for i in range(N):
     time.sleep(interval)
     print(f"Writing update number {i}")
     new_arr = np.random.random(shape) + 1j * np.random.random(shape)
-    #arr_client.write(uri, persist=False)
     arr_client.write(new_arr, persist=False)
--- receiver.py
+++ receiver.py
@@ -5,30 +5,29 @@ import asyncio
 from prefect.deployments import run_deployment
 
 def on_new_workflow(update):
-    print(f"New array detected: {update.key}")
-    uri = f"https://tiled.nsls2.bnl.gov/api/v1/metadata/tst/sandbox/ptycho/{update.key}"
-    print(f"uri is {uri}")
-    flow_run = trigger_specific_deployment(uri)
+    print(f"New array detected: {update}")
+    uri_in = f"https://tiled.nsls2.bnl.gov/api/v1/metadata/tst/sandbox/synaps/reconstructions/{update.key}"
+    uri_out = f"https://tiled.nsls2.bnl.gov/api/v1/metadata/tst/sandbox/synaps/segmentations"
+    flow_run = trigger_specific_deployment(uri_in, uri_out)
     print(f"Created flow run: {flow_run.name}")
     
 
 
-def trigger_specific_deployment(uri):
+def trigger_specific_deployment(uri_in, uri_out):
     # Trigger a deployment with a specific name and optional parameters
     flow_run = run_deployment(
         name="handle-uri/hxn-als-docker",
-        parameters={"uri": uri}
+        parameters={"uri_in": uri_in, "uri_out": uri_out}
     )
     return flow_run
 
 
 # To run the function:
 if __name__ == "__main__":
-    client = from_uri('https://tiled-staging.nsls2.bnl.gov')
-    pt = client['tst/sandbox/ptycho_test']
+    client = from_uri('https://tiled.nsls2.bnl.gov')
+    pt = client['tst/sandbox/synaps/reconstructions']
     sub = pt.subscribe()
     sub.child_created.add_callback(on_new_workflow)
-    #sub.child_created.add_callback(on_new_array)
     print("Listening for updates. Use Ctrl+C to stop....")
     sub.start()  # block
 
