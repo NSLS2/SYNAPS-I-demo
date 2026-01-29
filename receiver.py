27b7de864cacf312520185aca0c6c1d3f1223c0c
skarakuzu <karakuzu.seher@gmail.com>
2026-01-29 10:50:49 -0500

    modify the receiver to submit prefect job

--- receiver.py
+++ receiver.py
@@ -4,12 +4,13 @@ from tiled.client import from_uri
 import asyncio
 from prefect.deployments import run_deployment
 
-def on_new_array(update):
+def on_new_workflow(update):
     print(f"New array detected: {update.key}")
-    sub = update.child().subscribe()
-    sub.new_data.add_callback(on_array_data)
-    # Start from the first update.
-    sub.start_in_thread(start=1)
+    uri = f"https://tiled.nsls2.bnl.gov/api/v1/metadata/tst/sandbox/ptycho/{update.key}"
+    print(f"uri is {uri}")
+    flow_run = trigger_specific_deployment(uri)
+    print(f"Created flow run: {flow_run.name}")
+    
 
 
 def trigger_specific_deployment(uri):
@@ -20,20 +21,14 @@ def trigger_specific_deployment(uri):
     )
     return flow_run
 
-def on_array_data(update):
-    print(update.data())
-    print("Received something")
-    uri = "Hello Mars"
-    flow_run = trigger_specific_deployment(uri)
-    print(f"Created flow run: {flow_run.name}")
-
 
 # To run the function:
 if __name__ == "__main__":
     client = from_uri('https://tiled-staging.nsls2.bnl.gov')
     pt = client['tst/sandbox/ptycho_test']
     sub = pt.subscribe()
-    sub.child_created.add_callback(on_new_array)
+    sub.child_created.add_callback(on_new_workflow)
+    #sub.child_created.add_callback(on_new_array)
     print("Listening for updates. Use Ctrl+C to stop....")
     sub.start()  # block
 
