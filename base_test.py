67406590d814a4c8f5adb7305ac71dff2973580b
Eugene Matviychuk <ymatviych@bnl.gov>
2026-02-28 19:37:54 -0500

    FIX: perceived race condition in child_created vs new_data callbacks

--- base_test.py
+++ base_test.py
@@ -1,7 +1,7 @@
-from automap_hxn.utils import load_and_queue
+from automap_hxn.loading import load_and_queue
 
 if __name__ == "__main__":
     # This is a test script to run the load_and_queue function.
     json_path = "initial_scan_sim.json"
 
-    load_and_queue(json_path, 392446, remote_seg=False, proceed_fine_scans=False)
+    load_and_queue(json_path, 392446, remote_seg=True, proceed_fine_scans=False)
--- receiver.py
+++ receiver.py
@@ -1,11 +1,11 @@
 import os
 import time
-from tiled.client.stream import Subscription
+from tiled.client.stream import Subscription, LiveArrayData, LiveChildCreated
 from tiled.client import from_uri
 import time
 import pandas
 from concurrent.futures import ThreadPoolExecutor
-from automap_hxn.utils import analyze_data_from_arrays
+from automap_hxn.analysis import analyze_data_from_arrays
 
 
 URI_IN = os.getenv("URI_IN", "https://tiled.nsls2.bnl.gov/api/v1/metadata/tst/sandbox/eugene/synaps/reconstructions")
@@ -13,6 +13,7 @@ URI_OUT = os.getenv("URI_OUT", "https://tiled.nsls2.bnl.gov/api/v1/metadata/tst/
 
 # Cache metadata updates to match them with subsequent data updates.
 METADATA_UPDATES = {}
+SUBSCRIPTIONS = []
 
 writer_client = from_uri(URI_OUT)
 
@@ -23,29 +24,38 @@ def segmentation_function(data, metadata):
     # output is the output of segmentation
     # structured as {"box_number": [x0, x1, y0, y1]}
     print("Doing segmentation....")
-    output =  analyze_data_from_arrays(data, metadata)
+    output = analyze_data_from_arrays(data, metadata)
     print("Writing segmentation to tiled....")
-    writer_client.write_table(output, metadata)
-
-def on_new_dataset(update):
-    "This runs when *metadata* is updated and a new dataset is created."
-    print(f"New array detected : {update}")
-    path_parts = tuple(update.subscription.segments)  # e.g. ('tst', 'sandbox', ...)
-    METADATA_UPDATES[path_parts] = update
-    sub = update.child().subscribe()
-    sub.child_created.add_callback(run_segmentation)
-    sub.start_in_thread(max_size=100_000_000_000)
-    
-def run_segmentation(update):
-    "This runs when data has been uploaded for a dataset."
-    # Run a segmentation on the data.
-    print("New Data extracted...")
     breakpoint()
-    data = update.child().read()  # Extract the numpy array from the update.
-    path_parts = tuple(update.subscription.segments)  # e.g. ('tst', 'sandbox', ...)
-    update_parent = METADATA_UPDATES[path_parts[:-1]]
-    metadata = update_parent.metadata
-    executor.submit(segmentation_function, data, metadata)    
+    writer_client.write_table(output, metadata)
+    
+ 
+def on_new_dataset(update: LiveChildCreated):
+    "This runs when a new dataset is created in the root container."
+    path_parts = tuple(update.subscription.segments) + (update.key,)
+    print(f"New dataset created: {'/'.join(path_parts)}")
+    METADATA_UPDATES[path_parts] = update.metadata  # Cache the metadata for later use
+    sub = update.child().subscribe()
+    sub.child_created.add_callback(on_new_array)
+    sub.start_in_thread()
+    SUBSCRIPTIONS.append(sub)
+
+
+def on_new_array(update: LiveChildCreated):
+    "This runs when a new array is created in the container; may not have any data yet!"
+    print(f"New array created: {update.key}. Waiting for data to be uploaded...")
+    sub = update.child().subscribe()  # subscribe to the array to get data updates
+    sub.new_data.add_callback(run_segmentation)
+    sub.start_in_thread(start=0, max_size=100_000_000_000)  # large max_size for bigger images
+    SUBSCRIPTIONS.append(sub)
+
+
+def run_segmentation(update: LiveArrayData):
+    "This runs when data is uploaded to the array. The metadata is retrieved from "
+    "the cache and passed to the segmentation function."
+    path_parts = tuple(update.subscription.segments)
+    metadata = METADATA_UPDATES.get(path_parts[:-1], {})  # Get metadata for the parent dataset
+    executor.submit(segmentation_function, data=update.data(), metadata=metadata)   
 
 
 # To run the function:
