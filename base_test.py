f8c6a0aa43ef5fd84951260d25bd15463e82f0ef
Eugene Matviychuk <ymatviych@bnl.gov>
2026-02-28 20:51:54 -0500

    ENH: output is written to a table

--- base_test.py
+++ base_test.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     # This is a test script to run the load_and_queue function.
     json_path = "initial_scan_sim.json"
 
-    load_and_queue(json_path, 392446, remote_seg=True, proceed_fine_scans=False)
+    load_and_queue(json_path, 392456, remote_seg=True, proceed_fine_scans=False)
--- receiver.py
+++ receiver.py
@@ -4,6 +4,7 @@ from tiled.client.stream import Subscription, LiveArrayData, LiveChildCreated
 from tiled.client import from_uri
 import time
 import pandas
+import pyarrow
 from concurrent.futures import ThreadPoolExecutor
 from automap_hxn.analysis import analyze_data_from_arrays
 
@@ -19,15 +20,51 @@ writer_client = from_uri(URI_OUT)
 
 executor = ThreadPoolExecutor(max_workers=4)
 
-def segmentation_function(data, metadata):
+# def write_table_to_tiled(client, data: dict[str, pd.DataFrame], metadata):
+#     if not table := pyarrow.Table.from_pylist(data_cache)):
+#         return  # Nothing to write
+
+#     # Initialize the table and keep a reference to the client
+#     df_client = client.create_appendable_table(
+#         schema=schema,
+#         key="internal",
+#         metadata=metadata,
+#         access_tags=self.access_tags,
+#     )
+#     self._internal_tables[desc_name] = df_client
+
+
+def segmentation_function(data, metadata, path_parts):
     #TODO insert here the inference call
     # output is the output of segmentation
     # structured as {"box_number": [x0, x1, y0, y1]}
-    print("Doing segmentation....")
+    print("Running segmentation algorithm on new data...")
     output = analyze_data_from_arrays(data, metadata)
-    print("Writing segmentation to tiled....")
-    breakpoint()
-    writer_client.write_table(output, metadata)
+    n_boxes = sum(len(boxes) for boxes in output.values())
+    print(f"Segmentation complete. Found {n_boxes} box{'es' if n_boxes != 1 else ''}.")
+
+    # Write the output to Tiled
+    if output:
+        dataset_name, table_name = path_parts
+        try:
+            container = writer_client[dataset_name]
+        except KeyError:
+             container = writer_client.create_container(dataset_name,
+                                                        access_tags=["tst_sandbox"])
+        
+        for channel, boxes in output.items():
+            if not (table := pyarrow.Table.from_pandas(boxes)):
+                continue
+            table_client = container.create_appendable_table(
+                schema=table.schema,
+                key=channel,
+                metadata=metadata,
+                access_tags=["tst_sandbox"],
+            )
+            table_client.append_partition(0, table)
+
+    # writer_client.write_table(output, metadata)
+        print("Segmentation table written to Tiled.")
     
  
 def on_new_dataset(update: LiveChildCreated):
@@ -55,7 +92,7 @@ def run_segmentation(update: LiveArrayData):
     "the cache and passed to the segmentation function."
     path_parts = tuple(update.subscription.segments)
     metadata = METADATA_UPDATES.get(path_parts[:-1], {})  # Get metadata for the parent dataset
-    executor.submit(segmentation_function, data=update.data(), metadata=metadata)   
+    executor.submit(segmentation_function, data=update.data(), metadata=metadata, path_parts=path_parts[-2:])
 
 
 # To run the function:
